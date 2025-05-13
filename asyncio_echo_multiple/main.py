7f9d5c07686ac19fab482b206129c8e3324f388f
Ke Jie <chzealot@gmail.com>
2025-05-13 22:16:31 +0800

    feat: 支持多个实例
    Signed-off-by: Ke Jie <chzealot@gmail.com>

---
+++ asyncio_echo_multiple/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import json
+import asyncio
+import dingtalk_stream
+
+
+class HelloHandler(dingtalk_stream.GraphHandler):
+    async def process(self, callback: dingtalk_stream.CallbackMessage):
+        request = dingtalk_stream.GraphRequest.from_dict(callback.data)
+        body = json.loads(request.body)
+        await self.reply_markdown(body['sessionWebhook'], '- 天气：晴\n- temperature: 22')
+        return dingtalk_stream.AckMessage.STATUS_OK, self.get_success_response({'success': True}).to_dict()
+
+async def hello():
+    credential = dingtalk_stream.Credential(os.getenv('CLIENT_ID'), os.getenv('CLIENT_SECRET'))
+
+    client1 = dingtalk_stream.DingTalkStreamClient(credential)
+    client1.register_callback_handler(dingtalk_stream.graph.GraphMessage.TOPIC, HelloHandler())
+
+    client2 = dingtalk_stream.DingTalkStreamClient(credential)
+    client2.register_callback_handler(dingtalk_stream.graph.GraphMessage.TOPIC, HelloHandler())
+
+    await asyncio.gather(client1.start(), client2.start())
+
+if __name__ == '__main__':
+    asyncio.run(hello())
